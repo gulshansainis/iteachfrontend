605cc87ab02e2b3aa8ed30bc320ebce83ba277a1
Gulshan Saini <gulshan.saini@gmail.com>
2020-01-16 14:32:32 +0530

    added logo/ favicon and made responsive header

--- gatsby-config.js
+++ gatsby-config.js
@@ -87,5 +87,39 @@ module.exports = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "GatsbyJS",
+        short_name: "GatsbyJS",
+        start_url: "/",
+        background_color: "#6b37bf",
+        theme_color: "#6b37bf",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        crossOrigin: `use-credentials`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        // precachePages lets you specify pages whose resources should be precached
+        // by the service worker, using an array of globs. For example:
+        // precachePages: [`/about-us/`, `/projects/*`],
+      },
+    },
+    {
+      resolve: "gatsby-plugin-react-svg",
+      options: {
+        rule: {
+          include: /images/, // See below to configure properly
+        },
+      },
+    },
   ],
 }
