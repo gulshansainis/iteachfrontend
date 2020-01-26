deeee6a0f577c1c2b96c6d1cb074c9eab8b946d8
Gulshan Saini <gulshan.saini@gmail.com>
2020-01-26 22:18:59 +0530

    added gatsby image

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,8 @@ module.exports = {
     author: `@iteachfrontend`,
   },
   plugins: [
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
@@ -16,9 +18,25 @@ module.exports = {
       },
     },
     {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `markdown-pages`,
+        path: `${__dirname}/articles/images`,
+      },
+    },
+    {
       resolve: `gatsby-transformer-remark`,
       options: {
         plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              // It's important to specify the maxWidth (in pixels) of
+              // the content container as this plugin uses this as the
+              // base for generating different widths of each image.
+              maxWidth: 1080,
+            },
+          },
           {
             resolve: `gatsby-remark-prismjs`,
             options: {
